3f672d47264fedc2a17cd4e6a6e4b1f775fa7a14
pawalenavratna <117887016+pawalenavratna@users.noreply.github.com>
2022-12-07 17:39:47 +0530

    Add files via upload

---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/ServiceAgent/Builder/UserRolesLookupRequestBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Composition;
+using Retalix.Client.CommonServices.Utils;
+using Retalix.Contracts.Generated.Arts.PosLogV6.Source;
+using Retalix.Contracts.Generated.Common;
+using Retalix.Contracts.Generated.UserRoles;
+
+namespace Retalix.Sainsburys.Client.POSUI.Service_Agent.Builder
+{
+    /// <summary>
+    /// UserRoles Lookup Request Builder Class
+    /// </summary>
+    [Export(typeof(IUserRolesLookupRequestBuilder))]
+    public class UserRolesLookupRequestBuilder : IUserRolesLookupRequestBuilder
+    {
+        /// <summary>
+        /// UserRolesLookupServiceRequest class
+        /// </summary>
+        /// <param name="makerName"></param>
+        /// <returns></returns>
+        public UserRolesLookupServiceRequest BuildLookupRequest(string makerName)
+        {
+            var userRolesLookupRequest = new UserRolesLookupServiceRequest()
+            {
+                Header = new RetalixCommonHeaderType()
+                {
+                    MessageId = new RequestIDCommonData()
+                    {
+                        Value = MessageIdGenerator.GetId().ToString()
+                    }
+                }
+
+            };
+
+            return userRolesLookupRequest;
+        }
+    }
+}
