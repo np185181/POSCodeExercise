4968480a6bbb4536af80da079634fab01d3e87ca
pawalenavratna <117887016+pawalenavratna@users.noreply.github.com>
2022-12-07 17:26:24 +0530

    Add files via upload

---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/CommandHandlerInterfaces/IUserRolesInputCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using Retalix.Client.Common.Handlers;
+
+namespace Retalix.Sainsburys.Client.POSUI.CommandHandlerInterfaces
+{
+    /// <summary>
+    /// IUserRolesInputCommandHandler interface
+    /// </summary>
+    interface IUserRolesInputCommandHandler : ICommandHandler
+    {
+    }
+}
---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/CommandHandlerInterfaces/IUserRolesLookupCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Retalix.Client.Common.Handlers;
+
+namespace Retalix.Sainsburys.Client.POSUI.CommandHandlerInterfaces
+{
+    /// <summary>
+    /// IUserRolesLookupCommandHandler interface
+    /// </summary>
+    interface IUserRolesLookupCommandHandler : ICommandHandler
+    {
+        void Init(string carMakerName);
+    }
+}
