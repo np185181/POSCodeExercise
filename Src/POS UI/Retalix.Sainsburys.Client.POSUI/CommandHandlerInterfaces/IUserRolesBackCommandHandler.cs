f10fb89dfe1c35631b5f2ff6f9ce1e13cdb69f62
pawalenavratna <117887016+pawalenavratna@users.noreply.github.com>
2022-12-07 17:24:10 +0530

    Create  IUserRolesBackCommandHandler.cs

---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/CommandHandlerInterfaces/IUserRolesBackCommandHandler.cs
@@ -0,0 +1,11 @@
+using Retalix.Client.Common.Handlers;
+
+namespace Retalix.Sainsburys.Client.POSUI.CommandHandlerInterfaces
+{
+    /// <summary>
+    /// IUserRolesBackCommandHandler interface
+    /// </summary>
+    interface IUserRolesBackCommandHandler : ICommandHandler
+    {
+    }
+}
