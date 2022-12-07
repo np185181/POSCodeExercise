3e7126c1f0357f3042b32011e6c8c74068f10df0
pawalenavratna <117887016+pawalenavratna@users.noreply.github.com>
2022-12-07 17:40:55 +0530

    Add files via upload

---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/ServiceAgent/Service/UserRolesLookupService.cs
@@ -0,0 +1,17 @@
+﻿using Retalix.Client.POS.API.Infrastructure.Service;
+using Retalix.Contracts.Generated.UserRoles;
+
+namespace Retalix.Sainsburys.Client.POSUI.Service_Agent.Service
+{
+    /// <summary>
+    /// UserRolesLookupService
+    /// </summary>
+    public class UserRolesLookupService : ServiceBase, IUserRolesLookupService
+    {
+        private const string ServiceName = "UserRolesLookup";
+        public UserRolesLookupServiceResponse Execute(UserRolesLookupServiceRequest request)
+        {
+            return Execute<UserRolesLookupServiceRequest, UserRolesLookupServiceResponse>(request, ServiceName);
+        }
+    }
+}
