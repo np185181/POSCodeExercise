172bf2af41a58c6c58a0e60bdb557b1a2217ffb6
pawalenavratna <117887016+pawalenavratna@users.noreply.github.com>
2022-12-07 17:42:24 +0530

    Add files via upload

---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/ServiceAgent/IUserRolesLookupServiceAgent.cs
@@ -0,0 +1,13 @@
+﻿using Retalix.Client.Common.ServiceAgents;
+using Retalix.Contracts.Generated.UserRoles;
+
+namespace Retalix.Sainsburys.Client.POSUI.Service_Agent
+{
+    /// <summary>
+    /// IUserRolesLookupServiceAgent interface
+    /// </summary>
+    public interface IUserRolesLookupServiceAgent : IServiceAgent
+    {
+        UserRolesLookupServiceResponse Execute(string makerName);
+    }
+}
---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/ServiceAgent/UserRolesLookupServiceAgent.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Composition;
+using Retalix.Contracts.Generated.UserRoles;
+using Retalix.Sainsburys.Client.POSUI.Service_Agent.Builder;
+using Retalix.Sainsburys.Client.POSUI.Service_Agent.Service;
+using Retalix.Sainsburys.Client.POSUI.Service_Agent.Validator;
+
+namespace Retalix.Sainsburys.Client.POSUI.Service_Agent
+{
+    /// <summary>
+    /// Class for UserRoles Lookup Service Agent
+    /// </summary>
+    public class UserRolesLookupServiceAgent : IUserRolesLookupServiceAgent
+    {
+        [Import]
+        private IUserRolesLookupService _userRolesLookupService;
+
+        [Import]
+        private IUserRolesValidator _userRolesLookupValidator;
+
+        [Import]
+        private IUserRolesLookupRequestBuilder _userRolesLookupRequestBuilder;
+
+        /// <summary>
+        /// Execute method
+        /// </summary>
+        /// <param name="makerName"></param>
+        /// <returns></returns>
+        public UserRolesLookupServiceResponse Execute(string makerName)
+        {
+            var userRolesLookupRequest = _userRolesLookupRequestBuilder.BuildLookupRequest(makerName);
+            var userRolesLookupResponse = _userRolesLookupService.Execute(userRolesLookupRequest);
+            _userRolesLookupValidator.Validate(userRolesLookupRequest, userRolesLookupResponse);
+            
+            return userRolesLookupResponse;
+        }
+    }
+}
