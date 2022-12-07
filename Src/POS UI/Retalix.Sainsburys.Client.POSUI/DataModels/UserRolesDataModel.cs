4057f2ce7ebfa60e3fd74a015502d99db63c525d
pawalenavratna <117887016+pawalenavratna@users.noreply.github.com>
2022-12-07 17:33:24 +0530

    Add files via upload

---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/DataModels/UserRolesDataModel.cs
@@ -0,0 +1,17 @@
+﻿using Retalix.Contracts.Generated.UserRoles;
+
+namespace Retalix.Sainsburys.Client.POSUI.DataModels
+{
+    /// <summary>
+    /// UserRolesDataModel Class
+    /// </summary>
+    public class UserRolesDataModel
+    {
+        public UserRolesLookupServiceResponse userRolesLookupResponseType { get; set; }
+
+        public void Clear()
+        {
+            userRolesLookupResponseType = null;
+        }
+    }
+}
