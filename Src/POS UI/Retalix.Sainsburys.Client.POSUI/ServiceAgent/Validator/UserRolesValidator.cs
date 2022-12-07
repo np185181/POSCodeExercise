574a179266541ada8a4e395768213201cdad8dab
pawalenavratna <117887016+pawalenavratna@users.noreply.github.com>
2022-12-07 17:41:51 +0530

    Add files via upload

---
+++ Src/POS UI/Retalix.Sainsburys.Client.POSUI/ServiceAgent/Validator/UserRolesValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+using Retalix.Client.POS.BusinessObjects.ServiceAgents.Validations;
+using Retalix.Contracts.Generated.UserRoles;
+
+namespace Retalix.Sainsburys.Client.POSUI.Service_Agent.Validator
+{
+    /// <summary>
+    /// UserRolesValidator Class
+    /// </summary>
+    public class UserRolesValidator : RetalixValidatorBase, IUserRolesValidator
+    {
+        /// <summary>
+        /// Validate method to validate response error
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="response"></param>
+        [Export(typeof(IUserRolesValidator))]
+        public void Validate(UserRolesLookupServiceRequest request, UserRolesLookupServiceResponse response)
+        {
+            ValidateResponseError(response.Header);
+        }
+    }
+}
